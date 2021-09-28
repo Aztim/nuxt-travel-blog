8dddb07a1fbb4e9b8f557de8225db8b33ee1edac
Aztim <barboza-82@mail.ru>
2021-09-29 01:13:44 +0300

    auth.js

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -18,7 +18,7 @@ import Header from '~/components/Header.vue'
 import Footer from '~/components/Footer.vue'
 export default {
   components: { Header,Footer  },
-  // middleware: ['auth']
+  middleware: ['auth']
 }
 </script>
 
